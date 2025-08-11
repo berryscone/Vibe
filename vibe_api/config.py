cf8e35fed84aeae91ed6ac7cbc537af12ff623c0
양홍민 (Luke Yang) <hongmin.yang@lginnotek.com>
2025-08-11 16:45:14 +0900

    Refactor app and config to handle production and test case properly - separate DB for prod and test

--- vibe_api/config.py
+++ vibe_api/config.py
@@ -3,12 +3,25 @@ from dotenv import load_dotenv
 
 
 load_dotenv()
+
 database_uri = os.getenv("DATABASE_URI")
 if not database_uri:
     raise RuntimeError("DATABASE_URI not set in environment")
 
-class Config:
+test_database_uri = os.getenv("TEST_DATABASE_URI")
+if not test_database_uri:
+    raise RuntimeError("TEST_DATABASE_URI not set in environment")
+
+
+class ConfigProd:
     SQLALCHEMY_DATABASE_URI = database_uri
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_ECHO = False
+    # SQLALCHEMY_RECORD_QUERIES = False
+
+    
+class ConfigTest:
+    SQLALCHEMY_DATABASE_URI = test_database_uri
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = False
     # SQLALCHEMY_RECORD_QUERIES = False
